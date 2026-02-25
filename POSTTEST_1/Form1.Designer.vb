ac9a5b204b1937425b62157bbd030c8881c0f7c7
Ikhsan <hsanzxx9@gmail.com>
2026-02-26 00:45:36 +0800

    Add file posttest 1
    Pemrograman Visual - Posttest 1

---
+++ POSTTEST_1/Form1.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnTambah = New Button()
+        btnReset = New Button()
+        txtIPSemester = New TextBox()
+        lblIPSemester = New Label()
+        lblIPKText = New Label()
+        lblIPK = New Label()
+        lblPredikatText = New Label()
+        lblPredikat = New Label()
+        SuspendLayout()
+        ' 
+        ' btnTambah
+        ' 
+        btnTambah.Location = New Point(280, 294)
+        btnTambah.Name = "btnTambah"
+        btnTambah.Size = New Size(94, 29)
+        btnTambah.TabIndex = 0
+        btnTambah.Text = "Tambah"
+        btnTambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnReset
+        ' 
+        btnReset.Location = New Point(424, 294)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(94, 29)
+        btnReset.TabIndex = 1
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
+        ' 
+        ' txtIPSemester
+        ' 
+        txtIPSemester.Location = New Point(341, 235)
+        txtIPSemester.Name = "txtIPSemester"
+        txtIPSemester.Size = New Size(125, 27)
+        txtIPSemester.TabIndex = 2
+        ' 
+        ' lblIPSemester
+        ' 
+        lblIPSemester.AutoSize = True
+        lblIPSemester.Location = New Point(359, 96)
+        lblIPSemester.Name = "lblIPSemester"
+        lblIPSemester.Size = New Size(86, 20)
+        lblIPSemester.TabIndex = 3
+        lblIPSemester.Text = "IP Semester"
+        ' 
+        ' lblIPKText
+        ' 
+        lblIPKText.AutoSize = True
+        lblIPKText.Location = New Point(304, 153)
+        lblIPKText.Name = "lblIPKText"
+        lblIPKText.Size = New Size(89, 20)
+        lblIPKText.TabIndex = 4
+        lblIPKText.Text = "IP Kumulatif"
+        ' 
+        ' lblIPK
+        ' 
+        lblIPK.AutoSize = True
+        lblIPK.Location = New Point(304, 185)
+        lblIPK.Name = "lblIPK"
+        lblIPK.Size = New Size(36, 20)
+        lblIPK.TabIndex = 5
+        lblIPK.Text = "0.00"
+        ' 
+        ' lblPredikatText
+        ' 
+        lblPredikatText.AutoSize = True
+        lblPredikatText.Location = New Point(436, 153)
+        lblPredikatText.Name = "lblPredikatText"
+        lblPredikatText.Size = New Size(63, 20)
+        lblPredikatText.TabIndex = 6
+        lblPredikatText.Text = "Predikat"
+        ' 
+        ' lblPredikat
+        ' 
+        lblPredikat.AutoSize = True
+        lblPredikat.Location = New Point(436, 185)
+        lblPredikat.Name = "lblPredikat"
+        lblPredikat.Size = New Size(15, 20)
+        lblPredikat.TabIndex = 7
+        lblPredikat.Text = "-"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = My.Resources.Resources.WhatsApp_Image_2025_08_07_at_18_12_37
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(800, 450)
+        Controls.Add(lblPredikat)
+        Controls.Add(lblPredikatText)
+        Controls.Add(lblIPK)
+        Controls.Add(lblIPKText)
+        Controls.Add(lblIPSemester)
+        Controls.Add(txtIPSemester)
+        Controls.Add(btnReset)
+        Controls.Add(btnTambah)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents txtIPSemester As TextBox
+    Friend WithEvents lblIPSemester As Label
+    Friend WithEvents lblIPKText As Label
+    Friend WithEvents lblIPK As Label
+    Friend WithEvents lblPredikatText As Label
+    Friend WithEvents lblPredikat As Label
+
+End Class
---
+++ POSTTEST_1/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+
+    ' Variabel untuk menyimpan total dan jumlah semester
+    Dim totalIPSemester As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+
+        Dim nilaiIPSemester As Double
+
+        ' Validasi input angka
+        If Double.TryParse(txtIPSemester.Text, nilaiIPSemester) Then
+
+            ' Validasi rentang IP
+            If nilaiIPSemester >= 0 And nilaiIPSemester <= 4 Then
+
+                totalIPSemester += nilaiIPSemester
+                jumlahSemester += 1
+
+                Dim nilaiIPK As Double = totalIPSemester / jumlahSemester
+                lblIPK.Text = nilaiIPK.ToString("0.00")
+
+                ' Menentukan predikat IPK
+                If nilaiIPK >= 2.0 And nilaiIPK <= 2.75 Then
+                    lblPredikat.Text = "Cukup"
+                ElseIf nilaiIPK >= 2.76 And nilaiIPK <= 3.0 Then
+                    lblPredikat.Text = "Memuaskan"
+                ElseIf nilaiIPK >= 3.01 Then
+                    lblPredikat.Text = "Sangat Memuaskan"
+                Else
+                    lblPredikat.Text = "-"
+                End If
+
+                ' Kosongkan textbox setelah tambah
+                txtIPSemester.Clear()
+                txtIPSemester.Focus()
+
+            Else
+                MessageBox.Show("IP Semester harus antara 0 sampai 4")
+            End If
+
+        Else
+            MessageBox.Show("Masukkan angka yang valid!")
+        End If
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        ' Reset semua data
+        totalIPSemester = 0
+        jumlahSemester = 0
+        lblIPK.Text = "0.00"
+        lblPredikat.Text = "-"
+        txtIPSemester.Clear()
+        txtIPSemester.Focus()
+
+    End Sub
+End Class
